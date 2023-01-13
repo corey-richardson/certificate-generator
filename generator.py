583d85176a7508944acb736de9f740a6b24bf7d3
corey-richardson <richardsoncorey04@gmail.com>
2023-01-13 19:16:25 +0000

    whitespace formatting changes, ignored DeprecationWarnings (bore), added progress status to console log, added final "completion" output

--- generator.py
+++ generator.py
@@ -1,3 +1,6 @@
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning) 
+
 import pandas as pd
 from tkinter import filedialog as fd
 from PIL import Image, ImageDraw, ImageFont
@@ -13,10 +16,12 @@ def main():
     # information_df = pd.read_csv(information_file)
     
     information_df = pd.read_csv("information.csv")
+    num_rows = len(information_df.index)
     
-    for name in information_df.first_name:
-        print(name)
+    for i, name in enumerate(information_df.first_name):
+        print(f"Creating certificate for {name}..... {i+1}/{num_rows}")
         make_cert(name)
+    print("Finished creating certificates!")
 
 
 def make_cert(name):
@@ -31,13 +36,8 @@ def make_cert(name):
     
     text_width, _ = draw.textsize(name, font = font) 
     draw.text( # write on image
-            (
-                (img_width - text_width) / 2,
-                text_y_pos
-            ),
-            name,
-            font = font,
-            fill = "black")
+            ( (img_width - text_width) / 2, text_y_pos),
+            name, font = font, fill = "black")
 
     img.save("out/{}.png".format(name.lower())) # save completed certificate to out/
         
